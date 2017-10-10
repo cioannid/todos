68e51e71662b096d2c2858a19eb89dc0f2856395
Christos Ioannidis <christos.ioannidis@skroutz.gr>
2017-10-10 17:23:26 +0300

    Todos are assigned to logged in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009135428) do
+ActiveRecord::Schema.define(version: 20171010133400) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
